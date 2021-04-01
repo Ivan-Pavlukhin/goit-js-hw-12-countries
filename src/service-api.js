2e916cf7a55d14f8c2a71bfc113168128912f59a
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-04-02 00:28:37 +0300

    Add catch to fetch

--- src/service-api.js
+++ src/service-api.js
@@ -1,7 +1,9 @@
 export default function fetchCountryByName(countryName) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`).then(
-    response => {
+  return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+    .then(response => {
       return response.json();
-    },
-  );
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
